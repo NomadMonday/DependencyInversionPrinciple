c8f1318461b2e800630060587256ff061ecdd7bb
NomadMonday <NomadMonday@gmail.com>
2022-07-09 18:42:25 -0700

    Update parameters for ABCMessageSender, was missing ABCPerson input.

--- End/abc_message_sender.py
+++ End/abc_message_sender.py
@@ -1,7 +1,8 @@
 from abc import ABC
 from abc import abstractmethod
+from abc_person import ABCPerson
 
 class ABCMessageSender(ABC):
     @abstractmethod
-    def SendMessage(self, message: str) -> None:
+    def SendMessage(self, person: ABCPerson, message: str) -> None:
         pass
